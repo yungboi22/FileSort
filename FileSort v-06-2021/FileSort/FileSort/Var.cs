697e4fd2407ed93af3c34f09b789c8349a7ab45e
yungboi22 <61335097+penner223@users.noreply.github.com>
2023-06-06 18:56:00 +0200

    Changes on hierarchy
    Moving the 2021 filesort project into a seperated version folder

---
+++ FileSort v-06-2021/FileSort/FileSort/Var.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+using System.IO;
+
+
+namespace FileSort
+{
+    public static class Var
+    {
+        //Main Vars
+        public static string ScanDirectory = @"D:\All";
+        public static string EndDirectory = @"D:\All\Müll";
+        public static string ExtensionDirectory = @"D:\All\Random\Don't delete this folder\Extensions";
+        public static string FileForStaticFolders = @"D:\All\Random\Don't delete this folder\Info\StaticFolders.txt";
+
+        //Arrays
+        public static Extension[] FileExtensions;
+        public static string[] StaticDirectories;
+        public static string[] ExtensionFolderNames;
+
+        public static void Initialize()
+        {
+            //Define ExtensionFolderNames
+            string[] ExtensionFolderPaths = Directory.GetFiles(ExtensionDirectory);
+            ExtensionFolderNames = new string[ExtensionFolderPaths.Length];
+
+            for(int i = 0; i<ExtensionFolderPaths.Length;i++)
+            {
+                ExtensionFolderNames[i] = Path.GetFileName(ExtensionFolderPaths[i]);
+                ExtensionFolderNames[i] = Path.GetFileNameWithoutExtension(ExtensionFolderNames[i]);
+            }
+
+            //Define FileExtensions
+            FileExtensions = new Extension[ExtensionFolderNames.Length];
+
+            for(int i = 0; i<ExtensionFolderNames.Length; i++)
+                FileExtensions[i] = new Extension(ExtensionFolderNames[i]);
+
+            //Define static Directories
+            var lines = File.ReadAllLines(FileForStaticFolders).Where(arg => !string.IsNullOrWhiteSpace(arg));
+            File.WriteAllLines(FileForStaticFolders, lines);
+            StaticDirectories = File.ReadAllLines(FileForStaticFolders);
+        }
+    }
+
+    public class Extension
+    {
+        public string Name;
+        public string[] Endings;
+        private static string Location = Var.ExtensionDirectory;
+        
+        public Extension(string aName)
+        {
+            Name = aName;
+            string Path = Location + @"\" + Name + ".txt"; 
+            Endings = File.ReadAllLines(Path);
+        }
+
+    }
+}
